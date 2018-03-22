6cf7c68d06f784d764e3da90d032308454a1a8ef
李科笠 <13120168539@163.com>
2018-03-22 09:14:08 +0800

    替换测试框架为goconvey

--- main_test.go
+++ main_test.go
@@ -7,42 +7,45 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"encoding/json"
 	"gin-proto-test/example"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 // protoc build code =>
 // protoc HouseContractListInput.proto HouseContractListOutput.proto --go_out=../
 
 func TestProtoMarshal(t *testing.T) {
-	router := setupRouter()
-	w := httptest.NewRecorder()
-	body := &example.HouseContractListInput{KeyWordName: "搜索"}
-	inData, _ := proto.Marshal(body)
+	Convey("Proto请求测试", t, func() {
+		router := setupRouter()
+		w := httptest.NewRecorder()
+		body := &example.HouseContractListInput{KeyWordName: "搜索"}
+		inData, _ := proto.Marshal(body)
 
-	req, _ := http.NewRequest("POST", "/test", bytes.NewReader(inData))
-	req.Header.Set("Content-Type", "application/x-protobuf")
-	router.ServeHTTP(w, req)
+		req, _ := http.NewRequest("POST", "/test", bytes.NewReader(inData))
+		req.Header.Set("Content-Type", "application/x-protobuf")
+		router.ServeHTTP(w, req)
 
-	var output example.HouseContractListOutput
-	_ = proto.Unmarshal(w.Body.Bytes(), &output)
-
-	assert.Equal(t, 2, int(output.TotalRow))
+		var output example.HouseContractListOutput
+		_ = proto.Unmarshal(w.Body.Bytes(), &output)
+		So(int(output.TotalRow), ShouldEqual, 2)
+	})
 }
 
 func TestJsonMarshal(t *testing.T) {
-	router := setupRouter()
-	w := httptest.NewRecorder()
-	body := &example.HouseContractListInput{KeyWordName: "搜索"}
-	inData, _ := json.Marshal(body)
+	Convey("Json请求测试", t, func() {
+		router := setupRouter()
+		w := httptest.NewRecorder()
+		body := &example.HouseContractListInput{KeyWordName: "搜索"}
+		inData, _ := json.Marshal(body)
 
-	req, _ := http.NewRequest("POST", "/test", bytes.NewReader(inData))
-	req.Header.Set("Content-Type", "application/json")
-	router.ServeHTTP(w, req)
+		req, _ := http.NewRequest("POST", "/test", bytes.NewReader(inData))
+		req.Header.Set("Content-Type", "application/json")
+		router.ServeHTTP(w, req)
 
-	var output example.HouseContractListOutput
-	_ = json.Unmarshal(w.Body.Bytes(), &output)
+		var output example.HouseContractListOutput
+		_ = json.Unmarshal(w.Body.Bytes(), &output)
 
-	assert.Equal(t, 2, int(output.TotalRow))
+		So(2, ShouldEqual, int(output.TotalRow))
+	})
 }
